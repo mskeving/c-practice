d1ca8bc2d0291a6241e72e557dd2d6382406161d
Melissa Skevington <mskeving@gmail.com>
2015-04-02 10:34:54 -0700

    counting words correctly

--- wordcount/wordcount.c
+++ wordcount/wordcount.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, char** argv)
 {
@@ -11,43 +12,38 @@ int main(int argc, char** argv)
     int total_bytes = 0;
 
     for (int i = 1; i < argc; i++) {
-        printf("%s ", argv[i]);
-    }
-    for (int i = 1; i < argc; i++) {
         int res = 0;
         int word_count = 0;
         int line_count = 0;
         fp = fopen(argv[i], "r");
 
-        if (fp != NULL)
-        {
-            while (!feof(fp))
-            {
+        if (fp != NULL) {
+            while (!feof(fp)) {
                 res = fread(buffer, 1, (sizeof buffer)-1, fp);
-                for (int i=0; i < res; i++)
-                {
-                    if (buffer[i]== ' ')
-                    {
-                        /* Before you say anything, I know this is not the right way to count words.*/
+                for (int i=0; i < res; i++) {
+                    if ((isspace(buffer[i]) && !isspace(buffer[i+1])) ||
+                            (!isspace(buffer[i]) && (i+1) == res)) {
                         ++word_count;
                     }
-                    if (buffer[i]== '\n')
-                    {
+                    if (buffer[i]== '\n') {
                         ++line_count;
                     }
                 }
             }
             fclose(fp);
-            printf("\n %7d %7d %7d %s", line_count, word_count, res, argv[i]);
+
+            printf("%7d %7d %7d %s \n", line_count, word_count, res, argv[i]);
+
             total_words += word_count;
             total_lines += line_count;
             total_bytes += res;
         }
         else
-            printf("\n wc: %s: open: No such file or directory", argv[i]);
+            printf("wc: %s: open: No such file or directory \n", argv[i]);
     }
-    printf("\n");
-    printf("%7d %7d %7d total \n", total_lines, total_words, total_bytes);
+    if (argc > 2) {
+        printf("%7d %7d %7d total \n", total_lines, total_words, total_bytes);
+    }
 
     return 0;
 }
